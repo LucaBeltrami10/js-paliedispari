50e27d119a95522e139b4eeaec9b0184d7e3ad1e
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-20 15:27:08 +0100

    commenti per funzione

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 /* PARI E DISPARI */
 /* funzioni */
 
+/* funzione numero random: 
+funzione in cui come argomenti inserisco i numeri estremi tra cui generare random da generare
+return: numero casuale generato*/
+
+/* funzione Odd or Even:
+funzione in cui sommo due numeri e li divido per due:
+return: pari se il resto è zero
+return: dispari se il resto è !0 */
+
+
 /* creo funzione  */
 
 /* inserisco input utente per O or E, NUMERO */
